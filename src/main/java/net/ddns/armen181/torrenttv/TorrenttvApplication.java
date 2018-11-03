0f16c16d4f2ee0ae8b61e6d7b5d130f5667f2c89
armen <Armen.181@gmail.com>
2018-11-03 16:13:32 +0400

    init commit

---
+++ src/main/java/net/ddns/armen181/torrenttv/TorrenttvApplication.java
@@ -0,0 +1,12 @@
+package net.ddns.armen181.torrenttv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TorrenttvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TorrenttvApplication.class, args);
+    }
+}
---
+++ src/test/java/net/ddns/armen181/torrenttv/TorrenttvApplicationTests.java
@@ -0,0 +1,16 @@
+//package net.ddns.armen181.torrenttv;
+//
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//public class TorrenttvApplicationTests {
+//
+//    @Test
+//    public void contextLoads() {
+//    }
+//
+//}
